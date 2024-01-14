606b6d42aea627efe2d3d2360616331fb07c1668
Mohit-it-is <mohitkr.mnnit@gmail.com>
2024-01-15 00:20:02 +0530

    added react router

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+      <Header />
       <Text>Notes appp</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
